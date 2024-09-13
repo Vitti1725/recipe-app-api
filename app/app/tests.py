2891a9b708ea2f97f59a0aa63302ad70a2f43e5e
Vitti <vittigupta17@gmail.com>
2024-09-13 10:29:08 +0530

    Configured docker compose and checks to wait for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,11 +6,12 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res= calc.add(5,6)
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = calc.subtract(6,3)
-        self.assertEqual(res,3)
+        res = calc.subtract(6, 3)
+        self.assertEqual(res, 3)
